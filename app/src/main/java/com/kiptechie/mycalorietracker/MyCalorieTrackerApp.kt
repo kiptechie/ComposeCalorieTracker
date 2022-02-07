cced9eacbe165e9bd1391b06ce4f3f3fc224bead
Timothy Serem <theecodepoet@gmail.com>
2022-02-07 23:00:09 +0300

    preferences set up

---
+++ app/src/main/java/com/kiptechie/mycalorietracker/MyCalorieTrackerApp.kt
@@ -0,0 +1,7 @@
+package com.kiptechie.mycalorietracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyCalorieTrackerApp: Application()
---
+++ core/src/main/java/com/kiptechie/core/domain/preferences/Preferences.kt
@@ -0,0 +1,42 @@
+package com.kiptechie.core.domain.preferences
+
+import com.kiptechie.core.domain.models.ActivityLevel
+import com.kiptechie.core.domain.models.Gender
+import com.kiptechie.core.domain.models.GoalType
+import com.kiptechie.core.domain.models.UserInfo
+
+interface Preferences {
+
+    fun saveGender(gender: Gender)
+
+    fun saveAge(age: Int)
+
+    fun saveWeight(weight: Float)
+
+    fun saveHeight(height: Int)
+
+    fun saveActivityLevel(level: ActivityLevel)
+
+    fun saveGoalType(type: GoalType)
+
+    fun saveCarbRatio(ratio: Float)
+
+    fun saveProteinRatio(ratio: Float)
+
+    fun saveFatRatio(ratio: Float)
+
+    fun loadUserInfo(): UserInfo
+
+    companion object {
+        const val KEY_GENDER = "gender"
+        const val KEY_AGE = "age"
+        const val KEY_WEIGHT = "weight"
+        const val KEY_HEIGHT = "height"
+        const val KEY_ACTIVITY_LEVEL = "activity_level"
+        const val KEY_GOAL_TYPE = "goal_type"
+        const val KEY_CARB_RATIO = "carb_ratio"
+        const val KEY_PROTEIN_RATIO = "protein_ratio"
+        const val KEY_FAT_RATIO = "fat_ratio"
+    }
+
+}
