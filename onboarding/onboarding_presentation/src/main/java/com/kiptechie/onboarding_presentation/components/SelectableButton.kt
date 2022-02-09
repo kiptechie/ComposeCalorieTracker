f6618e3c6cf99b7547caa4961c095d55484db1e7
Timothy Serem <theecodepoet@gmail.com>
2022-02-09 09:26:41 +0300

    created selectable button and unit text field components

---
+++ onboarding/onboarding_presentation/src/main/java/com/kiptechie/onboarding_presentation/components/SelectableButton.kt
@@ -0,0 +1,55 @@
+package com.kiptechie.onboarding_presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import com.kiptechie.core_ui.LocalSpacing
+
+@Composable
+fun SelectableButton(
+    text: String,
+    isSelected: Boolean,
+    color: Color,
+    selectedTextColor: Color,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = MaterialTheme.typography.button
+) {
+    val dimens = LocalSpacing.current
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .clip(RoundedCornerShape(dimens.extraLarge))
+            .border(
+                width = 2.dp,
+                color = color,
+                shape = RoundedCornerShape(dimens.extraLarge)
+            )
+            .background(
+                color = if (isSelected) color else Color.Transparent,
+                shape = RoundedCornerShape(dimens.extraLarge)
+            )
+            .clickable {
+                onClick()
+            }
+            .padding(dimens.medium)
+    ) {
+        Text(
+            text = text,
+            style = textStyle,
+            color = if (isSelected) selectedTextColor else color,
+        )
+    }
+}
