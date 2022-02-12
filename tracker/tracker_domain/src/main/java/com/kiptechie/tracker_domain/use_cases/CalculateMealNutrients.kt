c0eab3023dd63e49e7c2434bc407c50c2e6e57cb
Timothy Serem <theecodepoet@gmail.com>
2022-02-13 01:06:31 +0300

    added a use case to calculate nutirents
    Signed-off-by: Timothy Serem <theecodepoet@gmail.com>

---
+++ tracker/tracker_domain/src/main/java/com/kiptechie/tracker_domain/use_cases/CalculateMealNutrients.kt
@@ -0,0 +1,103 @@
+package com.kiptechie.tracker_domain.use_cases
+
+import com.kiptechie.core.domain.models.ActivityLevel
+import com.kiptechie.core.domain.models.Gender
+import com.kiptechie.core.domain.models.GoalType
+import com.kiptechie.core.domain.models.UserInfo
+import com.kiptechie.core.domain.preferences.Preferences
+import com.kiptechie.tracker_domain.models.MealType
+import com.kiptechie.tracker_domain.models.TrackedFood
+import kotlin.math.roundToInt
+
+class CalculateMealNutrients(
+    private val preferences: Preferences
+) {
+
+    data class MealNutrients(
+        val carbs: Int,
+        val protein: Int,
+        val fat: Int,
+        val calories: Int,
+        val mealType: MealType
+    )
+
+    data class Result(
+        val carbsGoal: Int,
+        val proteinGoal: Int,
+        val fatGoal: Int,
+        val caloriesGoal: Int,
+        val totalCarbs: Int,
+        val totalProtein: Int,
+        val totalFat: Int,
+        val totalCalories: Int,
+        val mealNutrients: Map<MealType, MealNutrients>
+    )
+
+    operator fun invoke(trackedFoods: List<TrackedFood>): Result {
+        val allNutrients = trackedFoods
+            .groupBy { _trackedFoods ->
+                _trackedFoods.mealType
+            }
+            .mapValues { entry ->
+                val type = entry.key
+                val foods = entry.value
+                MealNutrients(
+                    carbs = foods.sumOf { it.carbs },
+                    protein = foods.sumOf { it.protein },
+                    fat = foods.sumOf { it.fat },
+                    calories = foods.sumOf { it.calories },
+                    mealType = type
+                )
+            }
+        val totalCarbs = allNutrients.values.sumOf { it.carbs }
+        val totalProteins = allNutrients.values.sumOf { it.protein }
+        val totalFats = allNutrients.values.sumOf { it.fat }
+        val totalCalories = allNutrients.values.sumOf { it.calories }
+
+        val userInfo = preferences.loadUserInfo()
+        val caloryGoal = dailyCaloryRequirement(userInfo)
+        val carbsGoal = (caloryGoal * userInfo.carbRatio / 4f).roundToInt()
+        val proteinGoal = (caloryGoal * userInfo.proteinRatio / 4f).roundToInt()
+        val fatGoal = (caloryGoal * userInfo.fatRatio / 9f).roundToInt()
+
+        return Result(
+            carbsGoal = carbsGoal,
+            proteinGoal = proteinGoal,
+            fatGoal = fatGoal,
+            caloriesGoal = caloryGoal,
+            totalCarbs = totalCarbs,
+            totalProtein = totalProteins,
+            totalFat = totalFats,
+            totalCalories = totalCalories,
+            mealNutrients = allNutrients
+
+        )
+    }
+
+    private fun bmr(userInfo: UserInfo): Int {
+        return when (userInfo.gender) {
+            is Gender.Male -> {
+                (66.47f + 13.75f * userInfo.weight +
+                        5f * userInfo.height - 6.75f * userInfo.age).roundToInt()
+            }
+            is Gender.Female -> {
+                (665.09f + 9.56f * userInfo.weight +
+                        1.84f * userInfo.height - 4.67 * userInfo.age).roundToInt()
+            }
+        }
+    }
+
+    private fun dailyCaloryRequirement(userInfo: UserInfo): Int {
+        val activityFactor = when (userInfo.activityLevel) {
+            is ActivityLevel.Low -> 1.2f
+            is ActivityLevel.Medium -> 1.3f
+            is ActivityLevel.High -> 1.4f
+        }
+        val caloryExtra = when (userInfo.goalType) {
+            is GoalType.LoseWeight -> -500
+            is GoalType.KeepWeight -> 0
+            is GoalType.GainWeight -> 500
+        }
+        return (bmr(userInfo) * activityFactor + caloryExtra).roundToInt()
+    }
+}
