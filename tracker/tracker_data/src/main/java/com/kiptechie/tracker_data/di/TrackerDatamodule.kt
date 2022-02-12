351dc8d7696c85f125e46ee6f7786276dfa97af4
Timothy Serem <theecodepoet@gmail.com>
2022-02-12 22:57:47 +0300

    done

---
+++ tracker/tracker_data/src/main/java/com/kiptechie/tracker_data/di/TrackerDatamodule.kt
@@ -0,0 +1,41 @@
+package com.kiptechie.tracker_data.di
+
+import com.kiptechie.tracker_data.remote.OpenFoodApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TrackerDatamodule {
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOpenFoodApi(
+        client: OkHttpClient
+    ): OpenFoodApi {
+        return Retrofit.Builder()
+            .baseUrl(OpenFoodApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
+            .build()
+            .create(OpenFoodApi::class.java)
+    }
+}
