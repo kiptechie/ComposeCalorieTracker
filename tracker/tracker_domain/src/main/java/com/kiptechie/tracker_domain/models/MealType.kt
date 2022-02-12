fc4babbd789cd264c88f2047ced1b724121a7540
Timothy Serem <theecodepoet@gmail.com>
2022-02-13 01:07:49 +0300

    added a meal type sealed class
    Signed-off-by: Timothy Serem <theecodepoet@gmail.com>

---
+++ tracker/tracker_domain/src/main/java/com/kiptechie/tracker_domain/models/MealType.kt
@@ -0,0 +1,22 @@
+package com.kiptechie.tracker_domain.models
+
+sealed class MealType(val name: String) {
+
+    object Breakfast : MealType("breakfast")
+    object Lunch : MealType("lunch")
+    object Dinner : MealType("dinner")
+    object Snack : MealType("snack")
+
+    companion object {
+        fun fromString(name: String): MealType {
+            return when (name) {
+                "breakfast" -> Breakfast
+                "lunch" -> Lunch
+                "dinner" -> Dinner
+                "snack" -> Snack
+                else -> Breakfast
+            }
+        }
+    }
+
+}
